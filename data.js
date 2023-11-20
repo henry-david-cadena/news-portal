540ca46125a50179a264b7a6ce48fa425b9df8b0
Henry David Cadena <henry.cadena@tarjeta.click>
2023-11-20 11:14:06 -0500

    News Portal Completion

---
+++ data.js
@@ -0,0 +1,45 @@
+const data = {
+    "status": "ok",
+    "totalResults": 12664,
+    "articles": [
+        {
+            "source": {
+                "id": null,
+                "name": "Barron's"
+            },
+            "author": "Allan Sloan",
+            "title": "Tesla Has Taken the S&P 500 on a Wild Ride to Nowhere",
+            "description": "CEO Elon Musk has presided over wild swings in Tesla’s stock price, leaving it just above where it started when it entered the S&P 500.",
+            "url": "https://www.barrons.com/articles/tesla-stock-price-musk-5e6c38ae",
+            "urlToImage": "https://images.barrons.com/im-83602516/social",
+            "publishedAt": "2023-11-17T17:02:00Z",
+            "content": "This is commentary by Allan Sloan, an independent journalist and seven-time winner of the Loeb Award, business journalisms highest honor. Tesla stock has made Elon Musk immensely wealthy. But it su… [+5330 chars]"
+        },
+        {
+            "source": {
+                "id": "politico",
+                "name": "Politico"
+            },
+            "author": "Kierra Frazier",
+            "title": "White House slams Elon Musk for supporting antisemitic post",
+            "description": "The White House said it was “unacceptable to repeat the hideous lie behind the most fatal act of Antisemitism in American history at any time.”",
+            "url": "https://www.politico.com/news/2023/11/17/white-house-elon-musk-00127808",
+            "urlToImage": "https://static.politico.com/b1/5f/2b28e4b9477bbcf06f7a6934ebc3/britain-ai-summit-96239.jpg",
+            "publishedAt": "2023-11-17T17:01:14Z",
+            "content": "White House spokesperson Andrew Bates said in a statement posted to X, formerly known as Twitter, that the White House condemns this abhorrent promotion of Antisemitic and racist hate in the stronges… [+741 chars]"
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Biztoc.com"
+            },
+            "author": "siliconvalley.com",
+            "title": "Backlash spreads over Elon Musk’s endorsement of antisemitic post",
+            "description": "By Dana Hull and Ian King | Bloomberg Fallout from an Elon Musk post endorsing antisemitic views continues to spread, with Tesla Inc. investors criticizing the billionaire and more advertisers fleeing his social media platform X. The European Commission joine…",
+            "url": "https://biztoc.com/x/8f82803127794874",
+            "urlToImage": "https://c.biztoc.com/p/8f82803127794874/s.webp",
+            "publishedAt": "2023-11-17T17:00:07Z",
+            "content": "By Dana Hull and Ian King | BloombergFallout from an Elon Musk post endorsing antisemitic views continues to spread, with Tesla Inc. investors criticizing the billionaire and more advertisers fleeing… [+309 chars]"
+        }
+    ]
+}
